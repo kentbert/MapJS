f9bc44ae6f6fc5e689f38ba04c47bb95bff00aed
kentbert <quentin.berthommier@ac-versailles.fr>
2018-04-23 10:11:40 +0200

    Initial release

---
+++ map.js
@@ -0,0 +1,9 @@
+  var map;
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 15,
+      center: new google.maps.LatLng(45.75,4.85),
+      mapTypeId: 'terrain',
+      disableDefaultUI: true,
+    });
+  }
