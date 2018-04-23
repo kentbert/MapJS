da01b0625bf997fe5f007c959c4e1ed3a64cbf6c
kentbert <36899495+kentbert@users.noreply.github.com>
2018-04-23 10:13:41 +0200

    Update markers.js

--- markers.js
+++ markers.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-  $.getJSON("https://api.jcdecaux.com/vls/v1/stations?apiKey=bd36deba0672e9f5273b4c8b7bffe3436d2cbb92&contract=lyon", function(daCaux) {
+  $.getJSON("https://api.jcdecaux.com/vls/v1/stations?apiKey=xxxxxxxxxxxxxxxxxxxxxxxx&contract=lyon", function(daCaux) {
 
       let markers = [];
       for (let i=0; i<daCaux.length; i++) {
