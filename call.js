91321bd0f02f96922d037f3a0b1543eec23cd3ec
kentbert <quentin.berthommier@ac-versailles.fr>
2018-04-29 11:32:48 +0200

    reworked

--- call.js
+++ call.js
@@ -1,5 +1,31 @@
-$( document ).ready(function() {
-  $.getJSON("https://api.jcdecaux.com/vls/v1/stations?apiKey=bd36deba0672e9f5273b4c8b7bffe3436d2cbb92&contract=lyon", function(daCaux) {
-    stationInfos(daCaux);
+$(document).ready(function() {
+  $.when(
+    $.getScript( "sliderino.js" ),
+    $.getScript( "markers.js" ),
+    $.Deferred(function( deferred ){
+        $( deferred.resolve );
+    })
+  ).done(function(data) {
+
+    ElSliderino();
+
+    $.getJSON("https://api.jcdecaux.com/vls/v1/stations?apiKey=bd36deba0672e9f5273b4c8b7bffe3436d2cbb92&contract=lyon", function(dataVelov) {
+
+      let markers = [];
+
+      for (let i=0; i<dataVelov.length; i++) {
+
+      let address = dataVelov[i].address;
+          status = dataVelov[i].status;
+          dispo = dataVelov[i].available_bikes;
+          lat = dataVelov[i].position.lat;
+          lng = dataVelov[i].position.lng;
+
+        MarkerStation(address, status, dispo, lat, lng);
+
+      };
+
+
+    });
   });
 });
--- sliderino.js
+++ sliderino.js
@@ -1,39 +1,44 @@
-$(".close").click(function(){
-    $(".containerslider").toggle();
-});
+function ElSliderino() {
 
-var slides = document.querySelectorAll("#slider .card");
-var currentSlide = 0;
+  $(".close").click(function(){
+      $(".containerslider").toggle();
+  });
 
-function nextCard() {
-    changeCard(currentSlide+1);
-}
+  let slides = document.querySelectorAll("#slider .card");
+  let currentSlide = 0;
 
-function previousCard() {
-    changeCard(currentSlide-1);
-}
-
-function changeCard(i) {
-    slides[currentSlide].className = "card inactive";
-    currentSlide = (i+slides.length)%slides.length;
-    slides[currentSlide].className = "card active";
-}
-
-var next = document.getElementById("next");
-var previous = document.getElementById("previous");
-
-next.onclick = function() {
-    nextCard();
-};
-previous.onclick = function() {
-    previousCard();
-};
-
-document.onkeyup=function(nav){
-  if(nav.which == 69) {
-    nextCard();
+  function nextCard() {
+      changeCard(currentSlide+1);
   }
-  else if(nav.which == 65) {
-    previousCard();
+
+  function previousCard() {
+      changeCard(currentSlide-1);
   }
+
+  function changeCard(i) {
+      slides[currentSlide].className = "card inactive";
+      currentSlide = (i+slides.length)%slides.length;
+      slides[currentSlide].className = "card active";
+  }
+
+  var next = document.getElementById("next");
+  var previous = document.getElementById("previous");
+
+  next.onclick = function() {
+      nextCard();
+  };
+
+  previous.onclick = function() {
+      previousCard();
+  };
+
+  document.onkeyup=function(nav){
+    if(nav.which == 69) {
+      nextCard();
+    }
+    else if(nav.which == 65) {
+      previousCard();
+    }
+  }
+
 }
